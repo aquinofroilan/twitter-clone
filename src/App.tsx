c6c88a72b24b9785e1f173b0fd36ef5c2dfdf106
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-14 16:15:17 +0800

    feat: update package.json with additional metadata and dependencies; refactor App component to include routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/auth"></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
