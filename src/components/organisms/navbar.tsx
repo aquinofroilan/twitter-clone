e8431c0aa86a18c4c19e03966701ffbaffb1abc4
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-14 22:45:59 +0800

    feat: add react-icons dependency and update NavBar component with icons and improved layout

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -1,9 +1,28 @@
 import { Link } from "react-router-dom";
-
+import { HiHome } from "react-icons/hi";
+import { IoMdSettings } from "react-icons/io";
+import { IoPersonCircleOutline } from "react-icons/io5";
 const NavBar = () => {
     return (
-        <nav style={{ background: "#111", padding: "1rem", color: "#fff" }}>
-            <Link to="/home">Feed</Link> |<Link to="/settings">Settings</Link> |<Link to="/profile">Profile</Link>
+        <nav className="flex justify-between gap-5">
+            <div className="flex justify-center items-center gap-3 flex-col">
+                <HiHome size={24} />
+                <Link className="text-black" to="/home">
+                    Feed
+                </Link>
+            </div>
+            <div className="flex justify-center items-center gap-3 flex-col">
+                <IoMdSettings size={24} />
+                <Link className="text-black" to="/settings">
+                    Settings
+                </Link>
+            </div>
+            <div className="flex justify-center items-center gap-3 flex-col">
+                <IoPersonCircleOutline size={24} />
+                <Link className="text-black" to="/profile">
+                    Profile
+                </Link>
+            </div>
         </nav>
     );
 };
