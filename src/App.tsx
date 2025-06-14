091fea699bb064b2de3481b53ff928ab6390a787
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-14 16:19:22 +0800

    revert changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/auth"></Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            <h1>Hello World</h1>
+        </>
     );
 }
 
