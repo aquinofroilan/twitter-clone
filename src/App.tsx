748d9882959d0157ed1bdfc788677c1fea22f053
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-14 15:18:21 +0800

    refactor: simplify App component to display a Hello World message

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import SignUpForm from "./components/features/auth/sign-up-form";
 
 function App() {
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card border-black border-2">
-                <SignUpForm />
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <h1>Hello World</h1>
         </>
     );
 }
