8173a224dc08594f70d50030a928c416944fcdf7
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-14 21:52:56 +0800

    scaffold basic pages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,6 @@
 export { default as LoginPage } from "./pages/auth/sign-in";
 export { default as SignUpPage } from "./pages/auth/sign-up";
+export { default as SearchPage } from "./pages/home/search";
+export { default as HomeFeed } from "./pages/home/feed";
+export { default as SettingsPage } from "./pages/home/settings";
+export { default as PeoplePage } from "./pages/home/people";
